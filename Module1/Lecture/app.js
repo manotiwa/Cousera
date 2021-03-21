534405c14f6edeb966d2079de0875a57fca857bc
Manoranjan Tiwari <7050711@gmail.com>
2021-03-21 11:51:18 +0530

    My First assignement

---
+++ Module1/Lecture/app.js
@@ -0,0 +1,26 @@
+var students={
+  name="",
+  type="student"
+};
+
+document.addEventListener('DOMContentLoaded', contentLoaded);
+
+function contentLoaded(event){
+  document.getElementById('name').addEventListener("keyup", KeyUp);
+}
+
+function KeyUp(event){
+  calculateNumericOutput();
+}
+
+function calculateNumericOutput(){
+  student.name=document.getElementById('name').value;
+
+  var totalNameValue=0;
+  for (var i=0; i< student.name.length; i++){
+    totalNameValue+=student.name.charCodeAt(i);
+  }
+//insert result into page
+var output ="Total Numeric value of person's name is " + totalNameValue;
+document.getElementById('output').innerText=output;
+}
