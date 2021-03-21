6ec73e261c1b01dac1261f55fde5a1b37b023ecb
Manoranjan Tiwari <7050711@gmail.com>
2021-03-21 16:17:03 +0530

    My First assignement

--- Module1/Assignement/js/app.js
+++ Module1/Assignement/js/app.js
@@ -1,25 +1,41 @@
-(function () {
+(
+function () {
     'use strict';
-
     var app = angular.module('LunchCheck', []);
 
     app.controller('LunchCheckController', LunchCheckController);
     LunchCheckController.$inject = ['$scope'];
     function LunchCheckController($scope) {
-        $scope.name;
-        $scope.message;
+		
+        $scope.name='';
+        $scope.message='';
+		$scope.green_opacity = '';
+        $scope.red_opacity = '';
+		
 		$scope.displayNumeric = function () {
 			
-			var lungimea = $scope.name.length;
-            console.log(lungimea);
-            if (lungimea == null)
+			var lunchitem = $scope.name.length;
+
+			    let $items = $scope.name.split(',');
+				let counter = 0;
+				
+                $items.forEach((item) => {
+					
+				if (item != false) {
+									counter++;
+									}
+                });
+            if (lunchitem == null || (lunchitem == 0 || counter==0))
             {
                 $scope.message = "Please enter data first";
-            } 
+				 $scope.red_opacity = '';
+                 $scope.green_opacity = '';
+            }
             else 
             {
-                $items = $scope.name.split(",");
-                $scope.message = $items.length <= 3? 'Enjoy!' : 'Too much!';
+                $scope.message = counter< 4? 'Enjoy!' : 'Too much!';
+				 $scope.green_opacity = counter<= 3? 'opaque' : '';
+				 $scope.red_opacity = counter > 3? 'opaque' :  '';				
             };
 			 
         };
