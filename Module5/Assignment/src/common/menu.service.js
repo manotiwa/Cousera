3b7b7c93a64a0805c8c149d15c080302adf02d5a
Manoranjan Tiwari <7050711@gmail.com>
2021-04-15 00:09:22 +0530

    My Fifth Assignement

--- Module5/Assignment/src/common/menu.service.js
+++ Module5/Assignment/src/common/menu.service.js
@@ -8,17 +8,7 @@ angular.module('common')
 MenuService.$inject = ['$http', 'ApiPath'];
 function MenuService($http, ApiPath) {
   var service = this;
-  service.user = {};
 
-  service.saveUser = function(user) {
-    service.user = angular.copy(user);
-    console.log(service.user);
-  }
-
-  service.getUser = function() {
-    return service.user;
-  }
-  
   service.getCategories = function () {
     return $http.get(ApiPath + '/categories.json').then(function (response) {
       return response.data;
@@ -37,12 +27,104 @@ function MenuService($http, ApiPath) {
     });
   };
 
-  service.getFavoriteDish = function(short_name) {
-    return $http.get(ApiPath + '/menu_items/' + short_name + '.json');
+
+  service.getMenuItems = function (shortName) {
+    var config = {};
+    if (shortName) {
+      config.params = {category: shortName};
+    }
+
+    return $http.get(ApiPath + '/menu_items.json', config).then(function (response) {
+      return response.data;
+    });
   }
+
+
+  service.getCategory = function (shortName) {
+    return $http.get(ApiPath + '/categories/' + shortName + '.json').then(function (response) {
+      return response.data;
+    });
+  };
+
+
+  service.getMenuItem = function(shortName) {
+    return $http.get(ApiPath + '/menu_items/' + shortName + '.json')
+    .then(function(response) {
+      return response.data;
+    });
+  };
+
+
+  service.saveMenuItem = function (menuItem) {
+    return $http.put(ApiPath + '/menu_items/' + menuItem.short_name, menuItem)
+    .then(function (response) {
+      return response.data;
+    });
+  };
 
 }
 
 
 
 })();
+
+
+
+
+
+
+
+
+
+
+
+// (function () {
+// "use strict";
+
+// angular.module('common')
+// .service('MenuService', MenuService);
+
+
+// MenuService.$inject = ['$http', 'ApiPath'];
+// function MenuService($http, ApiPath) {
+  // var service = this;
+  // service.user = {};
+
+  // service.saveUser = function(user) {
+    // service.user = angular.copy(user);
+    // console.log(service.user);
+  // }
+
+  // service.getUser = function() {
+    // return service.user;
+  // }
+  
+  // service.getCategories = function () {
+    // return $http.get(ApiPath + '/categories.json').then(function (response) {
+      // return response.data;
+    // });
+  // };
+
+
+  // service.getMenuItems = function (category) {
+    // var config = {};
+    // if (category) {
+      // config.params = {'category': category};
+    // }
+
+    // return $http.get(ApiPath + '/menu_items.json', config).then(function (response) {
+      // return response.data;
+    // });
+  // };
+
+  // service.getFavoriteDish = function(short_name) {
+    // return $http.get(ApiPath + '/menu_items/' + short_name + '.json');
+  // }
+
+// }
+
+
+
+// })();
+
+
